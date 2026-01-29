35be7c9cd26a1499a206446f9c3b71c0f0637efd
Cop2212 <129774517+Cop2212@users.noreply.github.com>
2026-01-29 13:53:40 +0700

    add age(users), lam bt 29/1/26 check 18+

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,12 +19,14 @@ class AuthController extends Controller
             'name'     => 'required|string|max:255',
             'email'    => 'required|email|unique:users,email',
             'password' => 'required|min:6',
+            'age'      => 'nullable|integer|min:0|max:150',
         ]);
 
         $user = User::create([
             'name'     => $data['name'],
             'email'    => $data['email'],
             'password' => Hash::make($data['password']),
+            'age'      => $data['age'] ?? null,
         ]);
 
         event(new Registered($user));
